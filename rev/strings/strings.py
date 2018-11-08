cfba874550db81e68890485bacad9f0a92fc4a6c
Hhro <kwonsw212@gmail.com>
2018-11-08 22:29:23 +0900

    FIRST COMMIT

--- rev/strings/strings.py
+++ rev/strings/strings.py
@@ -6,7 +6,8 @@ def l2s(l):
 
 def xor(l1,l2):
     res=[]
-    
+    base=len(l2)
+
     if type(l1)==str:
         l1=s2l(l1)
     if type(l2)==str:
@@ -14,10 +15,10 @@ def xor(l1,l2):
     if type(l1)!=list or type(l2)!=list:
         print '[REV ERROR]type should be list or string'
         return 
-    if len(l1)!=len(l2):
-        print '[REV ERROR]len of two string must be same'
+    if len(l1)<len(l2):
+        print '[REV ERROR]len of first argv should be longer than second one or same.'
         return
 
     for i in range(len(l1)):
-       res.append(l1[i]^l2[i])
+       res.append(l1[i]^l2[i%base])
     return res
