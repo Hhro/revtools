0c11204c9fb884c52789b49e406b5a4f93a4b7e6
Hhro <kwonsw212@gmail.com>
2018-11-09 12:13:29 +0900

    more

--- rev/strings/strings.py
+++ rev/strings/strings.py
@@ -4,21 +4,49 @@ def s2l(s):
 def l2s(l):
     return ''.join([chr(x) for x in l])
 
-def xor(l1,l2):
+def xor(s1,s2):
     res=[]
-    base=len(l2)
+    base=len(s1)
 
-    if type(l1)==str:
-        l1=s2l(l1)
-    if type(l2)==str:
-        l2=s2l(l2)
-    if type(l1)!=list or type(l2)!=list:
-        print '[REV ERROR]type should be list or string'
-        return 
-    if len(l1)<len(l2):
+    if type(s1)!=str or type(s2)!=str:
+        print '[REV ERROR] type should be str'
+        return
+    if len(s1)<len(s2):
         print '[REV ERROR]len of first argv should be longer than second one or same.'
         return
 
+    l1=s2l(s1)
+    l2=s2l(s2)
+
     for i in range(len(l1)):
        res.append(l1[i]^l2[i%base])
-    return res
+    return l2s(res)
+
+def lsh(s,op):
+    l=s2l(s)
+    res=[]
+
+    if type(s)!=str:
+        print '[REV ERROR]type should be string'
+        return
+
+    for elem in l:
+        res.append(elem<<op)
+    return l2s(res)
+
+def rsh(s,op):
+    l=s2l(s)
+    res=[]
+
+    if type(s)!=str:
+        print '[REV ERROR]type should be string'
+        return
+
+    for elem in l:
+        res.append(elem>>op)
+    return l2s(res)
+
+def rot(s,op):
+    l=s2l(s)
+    res=l[op:]+l[:op]
+    return l2s(res)
